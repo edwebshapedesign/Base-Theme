f650fa11011719c9f5645b4783344f8e562c1d5e
Ed Johnson <edjohnson@Eds-MacBook-Neo.local>
2026-07-31 12:58:22 +0100

    Updating theme files to remove Lumisol

--- header.php
+++ header.php
@@ -13,8 +13,7 @@
 	<meta name="thumbnail" content="<?php echo get_field('site_thumbnail', 'option'); ?>" />
   <meta content="width=device-width, initial-scale=1" name="viewport">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&family=Jost:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
   <link href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" rel="shortcut icon" type="image/x-icon">
   <link href="<?php echo get_template_directory_uri(); ?>/images/webclip.png" rel="apple-touch-icon">
